40247d04bad6a0fbbe23395f32063f4aaf5544b7
MBronko <michal.brono@protonmail.com>
2022-02-02 17:29:53 +0100

    Add fileserver app and .env

--- Broncloud/settings.py
+++ Broncloud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tpqchet+e8x--urc09xu1l2$cf+1m*$q6#efsdi@+2=n#!*&2$'
+SECRET_KEY = environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
+    'fileserver',
 ]
 
 MIDDLEWARE = [
